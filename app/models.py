1fc0339ae50a2393d9c9bf57d5bb36dbb61a5f4e
kokafor1 <kennedyokafor2003@gmail.com>
2024-03-31 23:21:49 -0400

    project level

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
+import secrets
 from . import db
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -9,17 +10,16 @@ class Task(db.Model):
     description = db.Column(db.String)
     completed = db.Column(db.Boolean, default=False)
     date_created = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    author = db.relationship('User', back_populates='tasks')
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.__set_password(kwargs.get('password', ''))
+        self.save()
 
     def __repr__(self):
-        return f"<User {self.id}>"
+        return f"<Task {self.id}|{self.title}>"
 
-    def __set_password(self, plaintext_password):
-        self.password = generate_password_hash(plaintext_password)
-        self.save()
 
     def save(self):
         db.session.add(self)
@@ -31,5 +31,60 @@ class Task(db.Model):
             'title': self.title,
             'description': self.description,
             'completed': self.completed,
-            'dateCreated': self.date_created
+            'dateCreated': self.date_created,
+            "author": self.author.to_dict()
         }
+    
+    def update(self, **kwargs):
+        allowed_fields = {'title', 'description'}
+
+        for key, value in kwargs.items():
+            if key in allowed_fields:
+                setattr(self, key, value)
+        self.save()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+    task = db.relationship('Task', back_populates='author')
+    token = db.Column(db.String, index=True, unique=True)
+    token_expiration = db.Column(db.DateTime(timezone=True))
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.__set_password(kwargs.get('password', ''))
+
+    def __repr__(self):
+        return f"<User {self.id} | {self.username}>"
+
+    def __set_password(self, plaintext_password):
+        self.password = generate_password_hash(plaintext_password)
+        self.save()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def check_password(self, plaintext_password):
+        return check_password_hash(self.password, plaintext_password)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "password": self.password,
+            "dateCreated": self.date_created
+        }
+
+    def get_token(self):
+        now = datetime.now(timezone.utc)
+        if self.token and self.token_expiration > now + timedelta(minutes=1):
+            return {"token": self.token, "tokenExpiration": self.token_expiration}
+        self.token = secrets.token_hex(16)
+        self.token_expiration = now + timedelta(hours=1)
+        self.save()
+        return {"token": self.token, "tokenExpiration": self.token_expiration}
--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,11 @@
 from flask import request
 from . import app, db
-from .models import Task
-from datetime import datetime
-from hw_data.tasks import tasks_list
+from .models import Task, User
+from .auth import basic_auth, token_auth
 
-@app.route('/tasks')
-def get_tasks():
-    if not request.is_json:
-        return {'error': 'Your content-type must be a json'}, 400
-    data = request.json
-    required_fields = ['title', 'description']
-    missing_fields = []
-    for field in required_fields:
-        if field not in data:
-            missing_fields.append(field)
-    if missing_fields:
-        return {'error': f"{', '.join(missing_fields)} must be in the request body"}, 400
-    
-    title = data.get('title')
-    description = data.get('description')
-
-    new_task = Task(title = title, description = description)
-
-    return new_task.to_dict(), 201
-
+@app.route('/')
+def index():
+    return "<h1>Hello World</h1>"
 
 @app.route('/tasks/<int:task_id>')
 def get_task(task_id):
@@ -45,18 +27,89 @@ def create_task():
             missing_fields.append(field)
     if missing_fields:
         return {'error': f"{', '.join(missing_fields)}must be in the description"},400
-    title = data['title']
-    description = data['description']
+    title = data.get['title']
+    description = data.get['description']
 
-    new_task = {
-        "id": len(tasks_list) + 1,
-        "title": data['title'],
-        "description": data['description'],
-        "completed": False,
-        "dateCreated": data.get('date_created')
-    }
+    current_user = token_auth.current_user()
 
-    tasks_list.append(new_task)
+    new_task = Task(title = title, description = description, user_id = current_user.id)
 
-    return data
+    return new_task.to_dict(), 201
 
+@app.route('/users', methods=['POST'])
+def create_user():
+    if not request.is_json:
+        return {'error': 'Your content-type must be application/json'}, 400
+    data = request.json
+
+    required_fields = ['username', 'email', 'password']
+    missing_fields = []
+    for field in required_fields:
+        if field not in data:
+            missing_fields.append(field)
+    if missing_fields:
+        return {'error': f"{', '.join(missing_fields)} must be in the request body"}, 400
+    
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+
+    check_users = db.session.execute(db.select(User).where( (User.username == username) | (User.email == email) )).scalars().all()
+    if check_users:
+        return {'error': "A user with that username and/or email already exists"}, 400
+    new_user = User(username = username , email = email, password = password)
+
+    return new_user.to_dict(), 201
+
+@app.route('/token')
+@basic_auth.login_required
+def get_token():
+    user = basic_auth.current_user()
+    return user.get_token()
+
+@app.route('/tasks')
+def get_tasks():
+    select_stmt = db.select(Task)
+    search = request.args.get('search')
+    if search:
+        select_stmt = select_stmt.where(Task.title.ilike(f"%{search}%"))
+    task = db.session.execute(select_stmt).scalars().all()
+    return [t.to_dict() for t in task]
+
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+@token_auth.login_required
+def edit_task(task_id):
+    if not request.is_json:
+        return {'error': 'You content-type must be application/json'}, 400
+    task = db.session.get(Task, task_id)
+    if task is None:
+        return {'error': f"Post with ID #{task_id} does not exist"}, 404
+    current_user = token_auth.current_user()
+    if current_user is not task.author:
+        return {'error': "This is not your post. You do not have permission to edit"}, 403
+    
+    data = request.json
+    task.update(**data)
+    return task.to_dict()
+
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+@token_auth.login_required
+def delete_task(task_id, user_id):
+    task = db.session.get(Task, task_id)
+    if task is None:
+        return {'error': f"Post {task_id} does not exist"}, 404
+    user = db.session.get(User, user_id )
+    if task.user_id != user.id:
+        return {'error' : f"Task #{task_id} is not associated with task #{user_id}"}, 403
+    current_user = token_auth.current_user()
+    if task.user != current_user:
+        return {'error': 'You do not have permission to delete this comment'}, 403
+    task.delete()
+    return {'success': "Task has been successfully deleted"}, 200
+
+@app.route('/users/<int:user_id>', methods=['DElETE'])
+@basic_auth.login_required
+def delete_user(user_id):
+    user = db.session.get(User, user_id)
+    user.delete()
+    return {'success': 'User has been deleted'}, 200
